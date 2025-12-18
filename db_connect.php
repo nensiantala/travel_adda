3745b2924908f9495ea2bc4afaf7dd93e19f9c56
Nensi Antala <146347257+nensiantala@users.noreply.github.com>
2025-12-18 13:51:51 +0530

    Change database connection details
    Updated database connection parameters for remote access.

--- db_connect.php
+++ db_connect.php
@@ -5,10 +5,10 @@ if ($conn->connect_error) {
 }
 ?>
 <?php
-$servername = "localhost";  // usually localhost
-$username = "root";         // your db username
-$password = "";             // your db password
-$dbname = "travel_adda";    // your database name
+$servername = "bpetbnfz8x1tmtklzzk0-mysql.services.clever-cloud.com";  // usually localhost
+$username = "uelp3faahgfbpa2c";         // your db username
+$password = "GolwS56KZiowaUMIdmob";             // your db password
+$dbname = "bpetbnfz8x1tmtklzzk0";    // your database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
